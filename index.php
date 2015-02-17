453944d642045c41a3887b6225cb9221b206de62
inCodeSpace <box4goodmail@gmail.com>
2015-02-17 22:41:16 +0300

    add3

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Andrey Test</title>
+</head>
+<body>
+	Hi! <br>
+	<?php
+		//echo "php work";
+		 phpinfo();
+	?>
+</body>
+</html>
+
